78851ebef451c2aae255bc13b2ae18ee43eefb46
Elvis Otieno <45665873+elvisDemo@users.noreply.github.com>
2023-09-01 11:55:06 +0300

    Update Plies.py
    Added indexing

--- Plies.py
+++ Plies.py
@@ -8,6 +8,7 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & 🥑Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
+my_fruit_list = my_fruit_list.set_index('Fruit')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
    
