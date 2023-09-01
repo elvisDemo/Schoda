805cfba27f9620806499c148219a397a6d1543b5
Elvis Otieno <45665873+elvisDemo@users.noreply.github.com>
2023-09-01 11:59:38 +0300

    Update Plies.py
    Edited the list select

--- Plies.py
+++ Plies.py
@@ -9,6 +9,6 @@ streamlit.text('🥗 Kale, Spinach & 🥑Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
-   
+streamlit.multiselect("Pick some fruits:",list('Fruit'))   
+streamlit.dataframe(my_fruit_list)
