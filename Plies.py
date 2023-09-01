12faf6920ceefa2de1dbdb8a9af0fdb3b8c93493
Elvis Otieno <45665873+elvisDemo@users.noreply.github.com>
2023-09-01 11:44:59 +0300

    Update Plies.py
    Utilizing panda dataframes

--- Plies.py
+++ Plies.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas
+
 
 streamlit.title('Strong Tower')
 streamlit.header('🍞Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & 🥑Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
    
