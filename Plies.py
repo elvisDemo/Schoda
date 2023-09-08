ceee4cebe3894693c0fc5bd651fe631edb9c2f6d
Elvis Otieno <45665873+elvisDemo@users.noreply.github.com>
2023-09-08 10:01:54 -0500

    Update Plies.py

--- Plies.py
+++ Plies.py
@@ -12,4 +12,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # my_fruit_list = my_fruit_list.set_index('Fruit')
 Showthis = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])   
 fruitsShow = my_fruit_list.loc(Showthis)
-streamlit.dataframe(fruitsShow)
+streamlit.dataframe(my_fruit_list)
