dc70cf2744f8467ea598c04e1020afc2bf2ba76d
Elvis Otieno <45665873+elvisDemo@users.noreply.github.com>
2023-09-10 21:11:08 +0300

    Update Plies.py

--- Plies.py
+++ Plies.py
@@ -16,7 +16,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-fruityvice_responser = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_responser = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.text(fruityvice_response)
 # my_fruit_list = my_fruit_list.set_index('Fruit')
 # Showthis = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])   
@@ -30,3 +34,4 @@ streamlit.text(fruityvice_responser.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_responser.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
