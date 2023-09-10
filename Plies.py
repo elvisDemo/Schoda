8b9dfc0ae551069789b9db5a7a618eabcaf5958c
Elvis Otieno <45665873+elvisDemo@users.noreply.github.com>
2023-09-10 20:58:42 +0300

    Update Plies.py

--- Plies.py
+++ Plies.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 #import snowflake.connector
 import requests
+import requests
 
 
 streamlit.title('Strong Tower')
@@ -15,6 +16,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 # my_fruit_list = my_fruit_list.set_index('Fruit')
 # Showthis = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])   
 # fruitsShow = my_fruit_list.loc(Showthis)
